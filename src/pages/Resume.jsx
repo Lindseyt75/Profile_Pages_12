a988fe6836617da93c002945faedca77831de3fe
Timothy Lindsey <lindseyt75@yahoo.com>
2025-02-07 00:32:55 -0500

    added resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <section id="resume">
       <h2>Resume</h2>
-      <a href="resume.pdf" download>Download Resume</a>
+      <a href="Tim_Lindsey_Resume.pdf" download>Download Resume</a>
       <ul>
         <li>JavaScript</li>
         <li>React</li>
