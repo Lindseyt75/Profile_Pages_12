244f7cf287885de58985aff5feac261f99a01194
Timothy Lindsey <lindseyt75@yahoo.com>
2025-02-11 22:45:58 -0500

    more projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -18,6 +18,28 @@ const Portfolio = () => {
           deployedLink="https://weather-service-1613.onrender.com"
           githubLink="https://github.com/Lindseyt75/Module9_Weather.git"
         />
+          <Project
+          title="Gamerz"
+          image="Gamerz.jpg"
+          deployedLink="https://gamerz-client.onrender.com/my-games"
+          githubLink="https://github.com/rvrutan/gamerz.git"
+        />
+          <Project
+          title="Employee Tracker"
+          image="Employeetracker.jpg"
+          githubLink="https://github.com/Lindseyt75/Employee_Tracker_Module10.git"
+        />
+                <Project
+          title="Project5"
+          image="Weatherapp2.jpg"
+          githubLink="https://github.com/Lindseyt75/Module9_Weather.git"
+        />
+                <Project
+          title="Project6"
+          image="Weatherapp2.jpg"
+          deployedLink="https://weather-service-1613.onrender.com"
+          githubLink="https://github.com/Lindseyt75/Module9_Weather.git"
+        />
         {/* Add more projects as needed */}
       </div>
     </section>
